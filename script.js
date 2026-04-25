873deed3e5eac3d840583bcc60dbee69566adbbe
leehs1120 <visxd1341@gmail.com>
2026-04-26 05:51:42 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const search = document.getElementById("search");
+const players = document.querySelectorAll(".player-card");
 
+search.addEventListener("keyup", function () {
+    const value = search.value.toLowerCase();
+
+    players.forEach(player => {
+        const name = player.innerText.toLowerCase();
+
+        if (name.includes(value)) {
+            player.style.display = "block";
+        } else {
+            player.style.display = "none";
+        }
+    });
+});
